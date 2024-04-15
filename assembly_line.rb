98cda5d201efb6f5e17a689bb6f6cc783f044ddf
Logan Cole <emtlogancole@gmail.com>
2024-04-15 11:11:55 -0500

    first draft

--- assembly_line.rb
+++ assembly_line.rb
@@ -4,10 +4,17 @@ class AssemblyLine
   end
 
   def production_rate_per_hour
-    raise 'Please implement the AssemblyLine#production_rate_per_hour method'
+    @speed * 221
   end
 
   def working_items_per_minute
-    raise 'Please implement the AssemblyLine#working_items_per_minute method'
+    return self.production_rate_per_hour if @speed < 5
+    if speed < 9
+      self.production_rate_per_hour * 0.9
+    elsif @speed == 9
+      self.production_rate_per_hour * 0.8
+    else
+      self.production_rate_per_hour * 0.77
+    end
   end
 end
